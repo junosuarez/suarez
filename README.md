6f6d80c3b1f33e22353cb4e6b5e7d08eb2515223
junosuarez <juno@suarez.email>
2019-02-23 13:09:09 -0800

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# suarez
+
+for use with npx. with [node installed](https://nodejs.org/en/download/current/):
+
+```
+> npx suarez
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+const lolcat = require('lolcatjs')
+
+lolcat.options.seed = Math.round(Math.random() * 1000)
+
+lolcat.fromString(`
+juno suárez
+
+http://suarez.systems
+
+npx suarez
+`)
